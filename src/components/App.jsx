dbc961ff9e6a67ab9d234159e2d035263fd13edd
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-09-23 09:36:43 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
-import { FeedbackOptions  } from "./FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
 import { Notification } from "./Notification/Notification";
 
